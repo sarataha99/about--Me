574fc89565ff19f0cc06b91850514630d2b2aa8b
zeinabqa <zina.qatr@gmail.com>
2021-04-28 16:01:04 +0300

    function

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 'use strict'
 let score =0 ;
+function userName(){
+
+
 let userName=prompt('please enter your name');
 console.log(' Hello ' + userName);
 alert(' Hello '+ userName )
+}
+userName();
+
+function me(){
+
 
 let me = prompt('Do I love Enemy movies?')
 console.log(me);
+
 
 switch (me.toLowerCase()) {
     case 'yes':
@@ -22,7 +31,9 @@ switch (me.toLowerCase()) {
             alert('please answer yes or no')
             break;
 }
-
+}
+me();
+function breaking(){
 let breaking = prompt('Do I love breaking bad movie?')
 console.log(breaking);
 
@@ -42,6 +53,9 @@ switch (breaking.toLowerCase()) {
         alert('please answer yes or no')
         break;
 }
+}
+breaking();
+function calm(){
 let calm = prompt('Do I love the calm?')
 console.log(calm);
 
@@ -61,7 +75,9 @@ switch (calm.toLowerCase()) {
             alert('please answer yes or no')
             break;
 }
-
+}
+calm();
+function dark(){
 let dark = prompt('Do I love dark movie?')
 console.log(dark);
 
@@ -81,6 +97,9 @@ switch (dark.toLowerCase()) {
         break;
 
 }
+}
+dark();
+function book(){
 let book = prompt('Do I love reading books?')
 console.log(book);
 
@@ -98,6 +117,8 @@ switch (book.toLowerCase()) {
     default:
         alert('please answer yes or no')
         break;}
+    }
+    book();
 
 //let score = 0;
 //let question6 = prompt('Do I Love Weaking Dead?');
@@ -115,7 +136,7 @@ switch (book.toLowerCase()) {
 
         break;*/
 
-
+function question6(){
 for (let i = 0; i < 4; i++) {
     let question6 = prompt('Do I Love Weaking Dead?');
     console.log(i)
@@ -216,7 +237,9 @@ switch (question7.toLowerCase()) {
 console.log ('your score is ' + score)
 
 
-
+}
+question6();
+function question7(){
 for (let j= 0; j < 6; j++) {
     let question7 = prompt('What Is My Fav Movie ? ', 'please choose Dark, weaking dead ,frinds , viking ');
     console.log(j)
@@ -275,5 +298,5 @@ console.log('130')
 
 //console.log(score);
 alert('your score is ' + score)
-
-    
+}
+question7();    
